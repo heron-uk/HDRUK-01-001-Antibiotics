b7f10986ce950bcca3f93ef3cd757520213ff945
Elin <elinrow@gmail.com>
2025-04-04 13:54:32 +0100

    Update to fix minor issues.

--- Study/Cohorts/GenerateCodelists.R
+++ Study/Cohorts/GenerateCodelists.R
@@ -23,29 +23,22 @@ ing_av <- cdm$concept %>%
 ingredient_desc <- getDrugIngredientCodes(
     cdm = cdm,
     name = ing_av,
-    type = "codelist",
-    nameStyle = "{concept_name}")
+    type = "codelist")
 
-if(isTRUE(primary_care) & numberRecords(cdm$visit_occurrence) > 0){
-cdm$patient_visit <- conceptCohort(
+if(isTRUE(restrict_to_inpatient) & numberRecords(cdm$visit_occurrence) > 0){
+cdm$inpatient_visit <- conceptCohort(
   cdm = cdm,
-  conceptSet = list(outpatient = 9202, outpatient_er = 9203, outpatient_hospital = 8756),
-  name = "patient_visit"
-)
-} else if(isTRUE(secondary_care) & numberRecords(cdm$visit_occurrence) > 0){
-cdm$patient_visit <- conceptCohort(
-  cdm = cdm,
-  conceptSet = list(inpatient = 9201, inpatient_er = 262),
-  name = "patient_visit"
+  conceptSet = list(inpatient = c(9201, 262)),
+  name = "inpatient_visit"
 ) 
-} else if (numberRecords(cdm$visit_occurrence) == 0) {
-  cdm$patient_visit <- cdm$visit_occurrence |>
-    compute(name = "patient_visit", temporary = FALSE)
+} else if (isTRUE(restrict_to_inpatient) & numberRecords(cdm$visit_occurrence) == 0) {
+  
+  cli::cli_text("No records in visit occurrence table - skip restriction to inpatients only")
 }
 
 ###
 # If there aren't any codelists in ing_all then the next steps are skipped.
-if (length(ingredient_desc) > 0 & numberRecords(cdm$patient_visit) > 0) {
+if (isTRUE(restrict_to_inpatient) & numberRecords(cdm$visit_occurrence) > 0) {
   
   # Creates cohort for watch list antibiotics for outpatients. Only include records 
   # of drug exposure on day of outpatient visit.
@@ -55,54 +48,37 @@ if (length(ingredient_desc) > 0 & numberRecords(cdm$patient_visit) > 0) {
     name = "prelim_antibiotics"
   ) %>%
     requireTableIntersect(
-      tableName = "patient_visit",
-      window = c(0, 0)
+      tableName = "inpatient_visit",
+      window = c(0, Inf),
+      indexDate = "cohort_start_date"
+    ) %>%
+    requireTableIntersect(
+      tableName = "inpatient_visit",
+      window = c(-Inf, 0),
+      indexDate = "cohort_end_date"
     ) %>%
     requireInDateRange(
       indexDate = "cohort_start_date",
       dateRange = study_period
     )
+} else {
+  cdm$prelim_antibiotics <- conceptCohort(
+    cdm = cdm,
+    conceptSet = ingredient_desc,
+    name = "prelim_antibiotics"
+  )  %>%
+    requireInDateRange(
+      indexDate = "cohort_start_date",
+      dateRange = study_period
+    )
+}
   
-  # Gets top ten antibiotics using record counts
-  counts <- cohortCount(cdm$prelim_antibiotics) |>
-    filter(number_records > min_cell_count) %>%
+  # Filter down to antibiotics with at least 100 records.
+counts <- cohortCount(cdm$prelim_antibiotics) |>
+    filter(number_records >= 100) %>%
     left_join(settings(cdm$prelim_antibiotics),
               by = "cohort_definition_id"
     )
   
   ingredient_desc <- ingredient_desc[names(ingredient_desc) %in%
                                            counts$cohort_name]
-  
-  sum_ingredients <- summariseCohortCount(cohort = cdm$prelim_antibiotics) %>%
-    filter(group_level %in% counts$cohort_name)
-  
-  results[["sum_ingredients"]] <- sum_ingredients
-  
-} else if (numberRecords(cdm$patient_visit) == 0) {
-  
-  cdm$prelim_antibiotics <- conceptCohort(
-    cdm = cdm,
-    conceptSet = ingredient_desc,
-    name = "prelim_antibiotics"
-  ) %>%
-    requireInDateRange(
-      indexDate = "cohort_start_date",
-      dateRange = study_period
-    )
-  
-  # Gets top ten antibiotics using record counts
-  counts <- cohortCount(cdm$prelim_antibiotics) |>
-    filter(number_records > min_cell_count) %>%
-    left_join(settings(cdm$prelim_antibiotics),
-              by = "cohort_definition_id"
-    )
-  
-  ingredient_desc <- ingredient_desc[names(ingredient_desc) %in%
-                                       counts$cohort_name]
-  
-  sum_ingredients <- summariseCohortCount(cohort = cdm$prelim_antibiotics) %>%
-    filter(group_level %in% counts_out$cohort_name)
-  
-  results[["sum_prelim_ingredients"]] <- sum_ingredients
-  
-}
