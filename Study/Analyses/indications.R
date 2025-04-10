31888f111c548008d5b35bf259ce59adf4192e19
Elin Rowlands <elinrow@gmail.com>
2025-04-10 11:01:25 +0100

    Update indications code.

--- Study/Analyses/indications.R
+++ Study/Analyses/indications.R
@@ -1,67 +1,20 @@
-#if (run_indications == TRUE) {
+if (run_indications == TRUE) {
   cli::cli_alert_info("- Getting indications")
 
-  indications <- c(
-    "sepsis",
-    "musculoskeletal infective disorder",
-    "endocarditis",
-    "infective disorder of head",
-    "genital infection",
-    "gastrointestinal infection",
-    "respiratory infection",
-    "urinary tract infectious disease",
-    "postprocedural infection"
-  )
-
-indication_codelists <- list()
-
-for(i in seq_along(indications)){
-    # Dynamically create the name and store the result in the list
-    indication_codelists[[indications[i]]] <- getCandidateCodes(
-      cdm,
-      keywords = c(indications[i]),
-      exclude = NULL,
-      domains = "Condition",
-      standardConcept = "Standard",
-      searchInSynonyms = FALSE,
-      searchNonStandard = FALSE,
-      includeDescendants = TRUE,
-      includeAncestor = FALSE
-    ) %>%
-      mutate(name = indications[i])
-}
-
-  # Combine the list of data frames into one large data frame
-  all_codes <- do.call(rbind, indication_codelists)
-
-  rownames(all_codes) <- NULL
-
-  all_codes_grouped <- all_codes %>%
-    group_by(name) %>%
-    summarise(
-      codes = list(concept_id),
-      code_count = n(),
-      .groups = "drop"
-    )
-
-  names_list <- setNames(all_codes_grouped$codes, all_codes_grouped$name)
-
-  #######
-
-  cdm$indications <- conceptCohort(
-    cdm = cdm,
-    conceptSet = names_list,
-    name = "indications"
-  )
+  indications <- read_csv("Cohorts/indications_concepts.csv")[,-1]
   
-  code_use <- list()
-  for(i in seq_along(names_list)){
-    code_use[[names(names_list)[i]]] <- summariseCohortCodeUse(names_list[i], 
-                                                              cdm = cdm, 
-                                                              cohortId = i,
-                                                                cohortTable = "indications")
-  }
-
+  #####
+  
+  indications_grouped <- indications %>%
+    group_by(indication_category) %>%
+    summarise(concept_id_vector = list(concept_id), .groups = "drop")
+  
+  indications_list <- setNames(indications_grouped$concept_id_vector, indications_grouped$indication_category)
+  
+  cdm$indications <- conceptCohort(cdm = cdm,
+                                   conceptSet = indications_list,
+                                   name = "indications")
+  
   indications_table <- cdm$antibiotics |>
     summariseCharacteristics(
       cohortIntersectFlag = list(
@@ -70,114 +23,15 @@ for(i in seq_along(indications)){
         )
       )
     )
-
-  results[["indications"]] <- indications_table
-
-  cli::cli_alert_success("- Got indications")
-#}
   
-#### Procedures
+  results[["summarise_indications"]] <- indications_table
   
-procedures <- c("cardiovascular surgical procedure")
-
-procedure_codelists <- list()
-
-for(i in seq_along(procedures)){
-  # Dynamically create the name and store the result in the list
-  procedure_codelists[[procedures[i]]] <- getCandidateCodes(
-    cdm,
-    keywords = c(procedures[i]),
-    exclude = NULL,
-    domains = "Procedure",
-    standardConcept = "Standard",
-    searchInSynonyms = FALSE,
-    searchNonStandard = FALSE,
-    includeDescendants = TRUE,
-    includeAncestor = FALSE
-  ) %>%
-    mutate(name = procedures[i])
+  for(i in seq_along(indications_list)){
+    results[[paste0("indication_code_use_", i)]] <- summariseCohortCodeUse(x = indications_list[i],
+                                                    cdm = cdm,
+                                                    cohortTable = "indications",
+                                                    cohortId = i)
+  }
+  
+  cli::cli_alert_info("- Got indications")
 }
-
-p_all_codes <- do.call(rbind, procedure_codelists)
-
-rownames(p_all_codes) <- NULL
-
-p_all_codes_grouped <- p_all_codes %>%
-  group_by(name) %>%
-  summarise(
-    codes = list(concept_id),
-    code_count = n(),
-    .groups = "drop"
-  )
-
-p_names_list <- setNames(p_all_codes_grouped$codes, p_all_codes_grouped$name)
-
-cdm$procedures <- conceptCohort(
-  cdm = cdm,
-  conceptSet = p_names_list,
-  name = "procedures"
-)
-
-procedures_table <- cdm$antibiotics |>
-  summariseCharacteristics(
-    cohortIntersectFlag = list(
-      "Indication Flag" = list(
-        targetCohortTable = "procedures", window = c(-7, 7)
-      )
-    )
-  )
-
-#### Symptoms
-
-symptoms <- c("fever",
-                "cough",
-                "nausea",
-              "vomiting",
-              "diarrhea")
-
-symptom_codelists <- list()
-
-for(i in seq_along(symptoms)){
-  # Dynamically create the name and store the result in the list
-  symptom_codelists[[symptoms[i]]] <- getCandidateCodes(
-    cdm,
-    keywords = c(symptoms[i]),
-    exclude = NULL,
-    domains = "Condition",
-    standardConcept = "Standard",
-    searchInSynonyms = FALSE,
-    searchNonStandard = FALSE,
-    includeDescendants = TRUE,
-    includeAncestor = FALSE
-  ) %>%
-    mutate(name = symptoms[i])
-}
-
-s_all_codes <- do.call(rbind, symptom_codelists)
-
-rownames(s_all_codes) <- NULL
-
-s_all_codes_grouped <- s_all_codes %>%
-  group_by(name) %>%
-  summarise(
-    codes = list(concept_id),
-    code_count = n(),
-    .groups = "drop"
-  )
-
-s_names_list <- setNames(s_all_codes_grouped$codes, s_all_codes_grouped$name)
-
-cdm$symptoms <- conceptCohort(
-  cdm = cdm,
-  conceptSet = s_names_list,
-  name = "symptoms"
-)
-
-symptoms_table <- cdm$antibiotics |>
-  summariseCharacteristics(
-    cohortIntersectFlag = list(
-      "Indication Flag" = list(
-        targetCohortTable = "symptoms", window = c(-7, 7)
-      )
-    )
-  )
